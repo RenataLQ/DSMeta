1b2adbe04b6f8bb7bd4c08e4ee4b8f40369e9aef
Renata LQ <renatagouveiaq@gmail.com>
2022-07-16 19:26:36 -0300

    Project created

---
+++ backend/src/test/java/com/RenataLQ/DSMeta/DsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RenataLQ.DSMeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
