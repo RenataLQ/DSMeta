9e66d623bd1aa2943d0d9858c57d1e72b25c4d36
Renata LQ <renatagouveiaq@gmail.com>
2022-07-16 20:00:36 -0300

    First component

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,101 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-container">
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
+                <div className="dsmeta-form-control-container">
+                    <DatePicker
+                        selected={new Date()}
+                        onChange={(date: Date) => { }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
+                </div>
+            </div>
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#001</td>
+                            <td className="show576">11/07/2022</td>
+                            <td>Harry Potter</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <td>R$ 55300.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#002</td>
+                            <td className="show576">11/07/2022</td>
+                            <td>Hermione Granger</td>
+                            <td className="show992">20</td>
+                            <td className="show992">15</td>
+                            <td>R$ 60500.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#003</td>
+                            <td className="show576">11/07/2022</td>
+                            <td>Ronald Weasley</td>
+                            <td className="show992">10</td>
+                            <td className="show992">9</td>
+                            <td>R$ 50000.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="show992">#004</td>
+                            <td className="show576">11/07/2022</td>
+                            <td>Neville Longbotton</td>
+                            <td className="show992">12</td>
+                            <td className="show992">8</td>
+                            <td>R$ 49900.00</td>
+                            <td>
+                                <div className="dsmeta-red-btn-container">
+                                    <NotificationButton />
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default SalesCard
